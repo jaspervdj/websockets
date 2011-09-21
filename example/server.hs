add89507b6328f5632852845e5fbc5be77fd6f61
Jasper Van der Jeugt <m@jaspervdj.be>
2011-09-21 21:41:53 +0200

    Document the example

--- example/server.hs
+++ example/server.hs
@@ -1,3 +1,5 @@
+-- | This is the Haskell implementation of the example for the WebSockets
+-- library. We implement a simple multi-user chat program.
 {-# LANGUAGE OverloadedStrings #-}
 import Data.Monoid (mappend)
 import Data.Text (Text)
@@ -9,24 +11,29 @@ import qualified Data.Text.IO as T
 
 import qualified Network.WebSockets as WS
 
+-- | We represent a client by his username and a 'WS.Sender'. We can use this
+-- sender to asynchronously send 'Text' to the client later.
 type Client = (Text, WS.Sender Text)
 
--- | State kept on the server
+-- | The state kept on the server is simply a list of connected clients. We've
+-- added an alias and some utility functions, so it will be easier to extend
+-- this state later on.
 type ServerState = [Client]
 
 -- | Create a new, initial state
 newServerState :: ServerState
 newServerState = []
 
--- | Number of active clients
+-- | Get the number of active clients
 numClients :: ServerState -> Int
 numClients = length
 
--- | Check if a user exists
+-- | Check if a user already exists (based on username)
 clientExists :: Client -> ServerState -> Bool
 clientExists client = any ((== fst client) . fst)
 
--- | Add a client
+-- | Add a client (first, you should verify the client is not already connected
+-- using 'clientExists')
 addClient :: Client -> ServerState -> ServerState
 addClient client clients = client : clients
 
@@ -34,58 +41,78 @@ addClient client clients = client : clients
 removeClient :: Client -> ServerState -> ServerState
 removeClient client = filter ((/= fst client) . fst)
 
--- | Send a message to all clients
-sendMessage :: Text -> ServerState -> IO ()
-sendMessage message clients = do
+-- | Send a message to all clients, and log it on stdout.
+broadcast :: Text -> ServerState -> IO ()
+broadcast message clients = do
     T.putStrLn message
     forM_ clients $ \(_, sender) -> sender WS.textData message
 
--- | Accepts clients, spawns a single handler for each one.
+-- | The main function first creates a new state for the server, then spawns the
+-- actual server. For this purpose, we use the simple server provided by
+-- 'WS.runServer'.
 main :: IO ()
 main = do
     state <- newMVar newServerState
     WS.runServer "0.0.0.0" 9160 $ do
-        -- Shake hands with the client, assume all is right
+        -- When a client connects, we first obtain the request. Once we have
+        -- that, we use the 'WS.handshake' function to generate a response,
+        -- which we finally send to the client.
+        --
+        -- We can use irrefutable pattern matches here because the server will
+        -- spawn a thread for each connection -- and a crashing thread does not
+        -- effect the rest of the program.
         Just rq <- WS.receiveRequest
         let Right rsp = WS.handshake rq
         WS.sendResponse rsp
 
-        -- When a client succesfully connects, read his user ID and add
-        -- him too the list
-        mline <- WS.receiveData
+        -- If we want to be able to send data to this client later, from another
+        -- thread, we obtain a sender. We will add this to the server state
+        -- later.
         sender <- WS.getSender
+
+        -- When a client is succesfully connected, we read the first message.
+        -- This should be in the format of "Hi, I am Jasper", where Jasper is
+        -- the requested username.
+        msg <- WS.receiveData
         clients <- liftIO $ readMVar state
-        case mline of
+        case msg of
             Nothing -> return ()
-            Just line
-                | not (prefix `T.isPrefixOf` line) ->
+            Just m
+                -- Check that the first message has the right format
+                | not (prefix `T.isPrefixOf` m) ->
                     WS.sendTextData ("Wrong announcement." :: Text)
+                -- Check that the given username is not already taken
                 | clientExists client clients ->
                     WS.sendTextData ("User already exists." :: Text)
+                -- All is right!
                 | otherwise -> do
-                    WS.sendTextData ("Welcome!" :: Text)
+                    -- We send a "Welcome!", according to our own little
+                    -- protocol. We add the client to the list and broadcast the
+                    -- fact that he has joined. Then, we give control to the
+                    -- 'talk' function.
                     WS.sendTextData ("Welcome!" :: Text)
                     liftIO $ modifyMVar_ state $ \s -> do
                         let s' = addClient client s
-                        sendMessage (fst client `mappend` " joined, " `mappend`
+                        broadcast (fst client `mappend` " joined, " `mappend`
                             T.pack (show (numClients s')) `mappend`
                             " users connected") s'
                         return s'
                     talk state client
               where
                 prefix = "Hi! I am "
-                client = (T.drop (T.length prefix) line, sender)
+                client = (T.drop (T.length prefix) m, sender)
 
--- Talks to the client (by echoing messages back) until EOF.
+-- | The talk function continues to read messages from a single client until he
+-- disconnects. All messages are broadcasted to the other clients.
 talk :: MVar ServerState -> Client -> WS.WebSockets ()
 talk state client@(user, _) = do
     msg <- WS.receiveData
     case msg of
         Nothing -> liftIO $ modifyMVar_ state $ \s -> do
             let s' = removeClient client s
-            sendMessage (user `mappend` " disconnected") s'
+            broadcast (user `mappend` " disconnected") s'
             return s'
         Just m -> do
-            liftIO $ readMVar state >>= sendMessage
+            liftIO $ readMVar state >>= broadcast
                 (user `mappend` ": " `mappend` m)
             talk state client

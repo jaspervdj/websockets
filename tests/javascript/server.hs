fb0d4b0d08f36e62626aec2f4f8ae76363e6e69e
Jasper Van der Jeugt <m@jaspervdj.be>
2011-09-30 12:53:26 +0200

    Update tests

--- tests/javascript/server.hs
+++ tests/javascript/server.hs
@@ -7,52 +7,53 @@ import Control.Monad.Trans (liftIO)
 import Data.ByteString (ByteString)
 import Data.ByteString.Lazy.Char8 ()
 import Data.Monoid (mappend)
+import Data.Text (Text)
 import qualified Data.Text.Lazy as TL
 
-import Network.WebSockets
+import qualified Network.WebSockets as WS
 
-echo :: WebSockets ()
+echo :: WS.WebSockets ()
 echo = do
-    msg <- receiveData
+    msg <- WS.receiveData
     liftIO $ putStrLn $ show (msg :: Maybe TL.Text)
     case msg of
-        Just m -> sendTextData m >> echo
+        Just m -> WS.sendTextData m >> echo
         _      -> return ()
 
-ping :: WebSockets ()
+ping :: WS.WebSockets ()
 ping = do
     forM_ ["Hai", "Come again?", "Right!"] $ \msg -> do
-        send controlMessage $ Ping msg
-        fr <- receiveMessage
+        WS.sendMessage $ WS.ping msg
+        fr <- WS.receiveMessage
         case fr of
-            Just (ControlMessage (Pong msg'))
+            Just (WS.ControlMessage (WS.Pong msg'))
                 | msg' == msg -> return ()
                 | otherwise   -> error "wrong message from client"
             _ -> error "ping: client closed socket too soon"
 
-    send dataMessage $ Text "OK"
+    WS.sendMessage $ WS.textData ("OK" :: Text)
 
-closeMe :: WebSockets ()
+closeMe :: WS.WebSockets ()
 closeMe = do
-    msg <- receiveData
+    msg <- WS.receiveData
     case (msg :: Maybe TL.Text) of
         Just "Close me!" -> return ()
         _                -> error "closeme: unexpected input"
 
-concurrentSend :: WebSockets ()
+concurrentSend :: WS.WebSockets ()
 concurrentSend = do
-    sender <- getSender
+    sender <- WS.getMessageSender
     liftIO $ do
         mvars <- mapM newMVar [1 :: Int .. 100]
         forM_ mvars $ \mvar -> forkIO $ do
             i <- readMVar mvar
-            sender textData $ "Herp-a-derp " `mappend` TL.pack (show i)
+            sender $ WS.textData $ "Herp-a-derp " `mappend` TL.pack (show i)
             _ <- takeMVar mvar
             return ()
         forM_ mvars $ flip putMVar 0
 
 -- | All tests
-tests :: [(ByteString, WebSockets ())]
+tests :: [(ByteString, WS.WebSockets ())]
 tests =
     [ ("/echo", echo)
     , ("/ping", ping)
@@ -62,15 +63,15 @@ tests =
 
 -- | Accepts clients, spawns a single handler for each one.
 main :: IO ()
-main = runServer "0.0.0.0" 8000 $ do
+main = WS.runServer "0.0.0.0" 8000 $ do
     -- Shake hands with the client, assume all is right
-    Just rq <- receiveRequest
-    let Right rsp = handshake rq
-    sendResponse rsp
+    Just rq <- WS.receiveRequest
+    let Right rsp = WS.handshake rq
+    WS.sendResponse rsp
 
     -- When a client succesfully connects, lookup the requested test and
     -- run it
-    let name = requestPath rq
+    let name = WS.requestPath rq
     liftIO $ putStrLn $ "Starting test " ++ show name
     let Just test = lookup name tests in test
     liftIO $ putStrLn $ "Test " ++ show name ++ " finished"

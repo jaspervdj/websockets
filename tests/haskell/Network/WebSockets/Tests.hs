9acd33de47e373f74651a2185d7ccc3638d03330
Jasper Van der Jeugt <m@jaspervdj.be>
2011-10-19 12:34:08 +0200

    Fix haskell tests

--- tests/haskell/Network/WebSockets/Tests.hs
+++ tests/haskell/Network/WebSockets/Tests.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
-module Network.WebSockets.Tests where
+module Network.WebSockets.Tests
+    ( tests
+    ) where
 
-import Control.Applicative ((<$>), (<*>))
+import Control.Applicative ((<$>))
 import Control.Monad (replicateM)
-import Control.Monad.Trans (liftIO)
 
 import Data.Attoparsec (Result (..), parse)
+import Data.Enumerator (($$))
 import Test.Framework (Test, testGroup)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import Test.QuickCheck (Arbitrary (..), Gen, Property, choose, elements, oneof)
@@ -14,48 +16,45 @@ import Test.QuickCheck.Monadic (assert, monadicIO, pick, run)
 import qualified Blaze.ByteString.Builder as Builder
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
-
+import qualified Data.Enumerator as E
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Encoding as TL
-import qualified Data.Enumerator as E
-import Data.Enumerator (($$))
 
 import Network.WebSockets
 import Network.WebSockets.Mask
 import Network.WebSockets.Monad
-import Network.WebSockets.Protocol (Protocol (..))
-import Network.WebSockets.Protocol.Hybi00 (Hybi00_ (..))
-import Network.WebSockets.Protocol.Hybi10 (Hybi10_ (..))
-import Network.WebSockets.Types
-import qualified Network.WebSockets.Decode as D
-import qualified Network.WebSockets.Encode as E
+import Network.WebSockets.Protocol
+import Network.WebSockets.Protocol.Hybi00
+import Network.WebSockets.Protocol.Hybi10
 import qualified Network.WebSockets.Protocol.Unsafe as Unsafe
 
 tests :: Test
 tests = testGroup "Network.WebSockets.Test"
-    [ testProperty "encodeFrameDecodeFrame-hybi10" (encodeFrameDecodeFrame Hybi10_)
-    , testProperty "encodeFrameDecodeFrame-hybi00" (encodeFrameDecodeFrameFor00 Hybi00_)
+    [ testProperty "encodeDecodeFrame-hybi10" (encodeDecodeFrame Hybi10_)
+    , testProperty "encodeDecodeFrame-hybi00" (encodeDecodeFrameHybi00 Hybi00_)
     , testProperty "fragmententation-hybi10" (fragmentation Hybi10_)
     ]
 
 -- | Encode a frame, then decode it again. We should obviously get our original
 -- frame back
-encodeFrameDecodeFrame :: Protocol p => p -> ArbitraryMask -> Frame -> Bool
-encodeFrameDecodeFrame proto (ArbitraryMask m) f =
+encodeDecodeFrame :: Protocol p => p -> ArbitraryMask -> Frame -> Bool
+encodeDecodeFrame proto (ArbitraryMask m) f =
     let lbs = Builder.toLazyByteString $ (encodeFrame proto) m f
         bs = B.concat $ BL.toChunks lbs
     in case parse (decodeFrame proto) bs of
         Done "" r -> f == r
-        err       -> error ("encodeFrameDecodeFrame: " ++ show err)
+        err       -> error ("encodeDecodeFrame: " ++ show err)
 
-encodeFrameDecodeFrameFor00 :: Protocol p => p -> ArbitraryMask -> ArbitraryFrameFor00 -> Bool
-encodeFrameDecodeFrameFor00 proto am (ArbitraryFrameFor00 f) = encodeFrameDecodeFrame proto am f
+encodeDecodeFrameHybi00 :: Protocol p
+                        => p -> ArbitraryMask -> ArbitraryFrameHybi00 -> Bool
+encodeDecodeFrameHybi00 proto am (ArbitraryFrameHybi00 f) =
+    encodeDecodeFrame proto am f
 
-newtype ArbitraryFrameFor00 = ArbitraryFrameFor00 Frame
+newtype ArbitraryFrameHybi00 = ArbitraryFrameHybi00 Frame
     deriving (Show)
 
-instance Arbitrary ArbitraryFrameFor00 where
-    arbitrary = ArbitraryFrameFor00 <$>
+instance Arbitrary ArbitraryFrameHybi00 where
+    arbitrary = ArbitraryFrameHybi00 <$>
         Frame True TextFrame <$> arbitraryUtf8
 
 newtype ArbitraryMask = ArbitraryMask Mask

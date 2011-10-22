49d92459f2eb40d5f650ec3ff2f8868c9dec5e75
Jasper Van der Jeugt <m@jaspervdj.be>
2011-10-22 14:07:20 +0200

    Add script to generate hpc program coverage

---
+++ coverage.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ghc -isrc -itests/haskell -fhpc --make tests/haskell/TestSuite.hs
+./tests//haskell/TestSuite
+mkdir -p tests/coverage
+hpc markup --destdir=tests/coverage --exclude=Main TestSuite.tix
+rm TestSuite.tix
+
+echo "Output written to tests/coverage/hpc_index.html"

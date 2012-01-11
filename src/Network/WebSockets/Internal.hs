55ca4f243b5c7f662fa1b0628299cc73dba0cefe
Jasper Van der Jeugt <m@jaspervdj.be>
2012-01-11 16:20:23 +0100

    Add Network.WebSockets.Internal module

---
+++ src/Network/WebSockets/Internal.hs
@@ -0,0 +1,9 @@
+-- | This module exports some extra functions. However, note that these
+-- utilities are primarily meant for internal use, and can change between minor
+-- releases.
+module Network.WebSockets.Internal
+    ( S.receiveEnum
+    , S.sendIter
+    ) where
+
+import qualified Network.WebSockets.Socket as S

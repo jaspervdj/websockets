7300ce2fc2f0dc1d5f0fd2e6cce264d30100c85f
Jasper Van der Jeugt <m@jaspervdj.be>
2013-02-23 11:25:40 +0100

    Update website to use Hakyll 4

--- web/site.hs
+++ web/site.hs
@@ -1,25 +1,37 @@
-{-# LANGUAGE Arrows, OverloadedStrings, ScopedTypeVariables #-}
+--------------------------------------------------------------------------------
+{-# LANGUAGE Arrows              #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
-import Control.Arrow ((>>>))
-import Control.Exception (IOException, catch)
-import Prelude hiding (catch)
-import System.Posix.Directory (changeWorkingDirectory)
-import System.Posix.Files (createSymbolicLink)
-import System.Process (rawSystem)
+module Main where
 
-import Hakyll
 
+--------------------------------------------------------------------------------
+import           Control.Exception      (SomeException, handle)
+import           Hakyll
+import           System.Posix.Directory (changeWorkingDirectory)
+import           System.Posix.Files     (createSymbolicLink)
+import           System.Process         (rawSystem)
+
+
+--------------------------------------------------------------------------------
 createSymbolicLink' :: FilePath -> FilePath -> IO ()
-createSymbolicLink' dst src = createSymbolicLink dst src `catch`
-    \(_ :: IOException) -> putStrLn $ "Could not link " ++
-        src ++ " -> " ++ dst ++ ", perhaps link already exists?"
+createSymbolicLink' dst src = handle handler $ createSymbolicLink dst src
+  where
+    handler :: SomeException -> IO ()
+    handler _ = putStrLn $ "Could not link " ++ src ++ " -> " ++ dst ++
+        ", perhaps link already exists?"
 
+
+--------------------------------------------------------------------------------
 makeLinks :: IO ()
 makeLinks = do
     createSymbolicLink' "../README"                   "README.markdown"
     createSymbolicLink' "../example/server.lhs"       "example.lhs"
     createSymbolicLink' "../dist/doc/html/websockets" "reference"
 
+
+--------------------------------------------------------------------------------
 makeHaddock :: IO ()
 makeHaddock = do
     putStrLn "Generating documentation..."
@@ -31,37 +43,42 @@ makeHaddock = do
     -- Ignore exit code
     sh c as = rawSystem c as >>= \_ -> return ()
 
-pageCompiler' :: Compiler Resource (Page String)
-pageCompiler' =
-    pageCompiler >>>
-    applyTemplateCompiler "templates/default.html" >>>
-    relativizeUrlsCompiler
 
+--------------------------------------------------------------------------------
+pageCompiler :: Compiler (Item String)
+pageCompiler =
+    pandocCompiler                                               >>=
+    loadAndApplyTemplate "templates/default.html" defaultContext >>=
+    relativizeUrls
+
+
+--------------------------------------------------------------------------------
 main :: IO ()
-main = do
-    makeLinks
-    hakyllWith config $ do
-        match "README.markdown" $ do
-            route $ customRoute $ \_ -> "index.html"
-            compile pageCompiler'
-        match "example.lhs" $ do
-            route $ setExtension "html"
-            compile pageCompiler'
-        match "reference/*" $ do
-            route idRoute
-            compile $ proc x -> do
-                () <- require "haddock" (\() () -> ()) -< ()
-                copyFileCompiler -< x
+main = hakyllWith config $ do
+    preprocess $ do
+        makeLinks
+        makeHaddock
 
-        -- | A virtual target which generates the documentation
-        create "haddock" $ unsafeCompiler $ const makeHaddock
+    match "README.markdown" $ do
+        route $ customRoute $ \_ -> "index.html"
+        compile pageCompiler
 
-        match "templates/*" $ compile templateCompiler
-        match "css/*" $ do
-            route idRoute
-            compile compressCssCompiler
+    match "example.lhs" $ do
+        route $ setExtension "html"
+        compile pageCompiler
+
+    match "reference/*" $ do
+        route idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route idRoute
+        compile compressCssCompiler
+
+    match "templates/*" $
+        compile templateCompiler
   where
-    config = defaultHakyllConfiguration
+    config = defaultConfiguration
         { deployCommand = "rsync --checksum -ave 'ssh -p 2222' _site/* \
                           \jaspervdj@jaspervdj.be:jaspervdj.be/websockets"
         }

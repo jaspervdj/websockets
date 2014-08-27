d8b2907ed50ea4b6e59b8dd3889ebdfe212458ae
Jasper Van der Jeugt <m@jaspervdj.be>
2014-08-27 19:19:57 +0200

    Bump cabal file

--- benchmarks/ping.hs
+++ benchmarks/ping.hs
@@ -1,18 +1,26 @@
+--------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+
+--------------------------------------------------------------------------------
 import Control.Monad (forever)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
-
 import qualified Network.WebSockets as WS
 
-ping :: WS.Request -> WS.WebSockets WS.Hybi00 ()
-ping rq = do
-    WS.acceptRequest rq
-    WS.send $ WS.textData ("Ping 0" :: B.ByteString)
-    forever $ do
-        msg <- WS.receiveData
-        let n = read (BC.unpack (B.drop 5 msg)) :: Int
-        WS.send $ WS.textData $ BC.pack $ "Ping " ++ show (n + 1)
 
+--------------------------------------------------------------------------------
+ping :: WS.ServerApp
+ping pending = do
+    conn <- WS.acceptRequest pending
+    WS.sendTextData conn ("Ping 0" :: B.ByteString)
+    forever $ do
+        msg <- WS.receiveData conn
+        let n = read (BC.unpack (B.drop 5 msg)) :: Int
+        WS.sendTextData conn $ BC.pack $ "Ping " ++ show (n + 1)
+
+
+--------------------------------------------------------------------------------
 main :: IO ()
 main = WS.runServer "0.0.0.0" 8088 ping

66c1c0487366c49274426354cf9a0a27d5c6c683
Jasper Van der Jeugt <m@jaspervdj.be>
2017-04-09 12:19:03 +0200

    Bump travis.yml

--- tests/autobahn/autobahn.sh
+++ tests/autobahn/autobahn.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/bash
 set -o errexit -o pipefail
 
+# Finding the right python
+if command -v python2.7; then
+    AUTOBAHN_PYTHON="python2.7"
+else
+    AUTOBAHN_PYTHON="python"
+fi
+
 # Note that this script will be executed from the project root.
 AUTOBAHN_ENV="$HOME/pyenvs/autobahn"
 echo "Setting up virtualenv..."
 if [[ ! -e "$AUTOBAHN_ENV" ]]; then
-    virtualenv "$AUTOBAHN_ENV"
+    virtualenv --python="$AUTOBAHN_PYTHON" "$AUTOBAHN_ENV"
     source "$AUTOBAHN_ENV/bin/activate"
     pip install autobahntestsuite
 else

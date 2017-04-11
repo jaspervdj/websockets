2e68caab3b767a7ec9d41f4d87014a00bf618a2f
Jasper Van der Jeugt <m@jaspervdj.be>
2017-04-11 19:04:02 +0200

    Check validity of close messages

--- src/Network/WebSockets/Extensions/StrictUnicode.hs
+++ src/Network/WebSockets/Extensions/StrictUnicode.hs
@@ -5,7 +5,8 @@ module Network.WebSockets.Extensions.StrictUnicode
 
 
 --------------------------------------------------------------------------------
-import           Control.Exception                         (throwIO)
+import           Control.Exception             (throwIO)
+import qualified Data.ByteString.Lazy          as BL
 import           Network.WebSockets.Extensions
 import           Network.WebSockets.Types
 
@@ -25,4 +26,12 @@ strictParse Nothing = return Nothing
 strictParse (Just (DataMessage rsv1 rsv2 rsv3 (Text bl _))) = do
     txt <- decodeUtf8Strict bl >>= either throwIO return
     return (Just (DataMessage rsv1 rsv2 rsv3 (Text bl (Just txt))))
+strictParse (Just msg@(ControlMessage (Close _ bl))) = do
+    -- If there is a body, the first two bytes of the body MUST be a 2-byte
+    -- unsigned integer (in network byte order) representing a status code with
+    -- value /code/ defined in Section 7.4.  Following the 2-byte integer, the
+    -- body MAY contain UTF-8-encoded data with value /reason/, the
+    -- interpretation of which is not defined by this specification.
+    decodeUtf8Strict (BL.drop 2 bl) >>= either throwIO (\_ -> return ())
+    return (Just msg)
 strictParse (Just msg) = return (Just msg)
